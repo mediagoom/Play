f739dd293b85d7f129807795902dc7b54e0cea25
aseduto <aseduto@hotmail.com>
2018-08-28 15:36:17 +0200

    switch to rollup and babel 7.0

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import babel from 'rollup-plugin-babel';
+
+
+export default {
+  external: ['dashjs']
+  , input: 'src/main.js'
+  , output: {
+      file: 'lib/bundle.js'
+    , sourcemap: true
+    , format: 'iife'
+    , name: 'mgPlay'
+    , globals: {
+        'dashjs': 'dashjs'
+      }
+  }
+  
+  , plugins: [
+    resolve({
+        preferBuiltins: true
+        , browser: true
+    })
+    , commonjs()
+    , builtins()
+    , globals()
+    , babel({
+          exclude: 'node_modules/**'
+        , babelrc: false
+        , presets: [['@babel/env', { modules: false }]]
+        //, plugins: ['@babel/external-helpers']
+        //, externalHelpers: true
+      })
+  ]
+};
