839816977451fdae8b0d2dbb8bcb1b71f7b8ac3a
aseduto <aseduto@hotmail.com>
2018-09-20 17:43:16 +0200

    added cjs bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,59 @@ import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 import babel from 'rollup-plugin-babel';
 
-
-export default {
-  external: ['dashjs']
-  , input: 'src/main.js'
-  , output: {
-      file: 'lib/bundle.js'
-    , sourcemap: true
-    , format: 'iife'
-    , name: 'mgPlay'
-    , globals: {
-        'dashjs': 'dashjs'
-      }
-  }
-  
-  , plugins: [
-    resolve({
-        preferBuiltins: true
-        , browser: true
-    })
-    , commonjs()
-    , builtins()
-    , globals()
-    , babel({
-          exclude: 'node_modules/**'
-        , babelrc: false
-        , presets: [['@babel/env', { modules: false }]]
-        , plugins: ['@babel/plugin-transform-object-assign']
-        //, externalHelpers: true
-      })
-  ]
+const gglobals = {
+  'dashjs': 'dashjs'
 };
+
+const gplugins = [
+  resolve({
+      preferBuiltins: true
+      , browser: true
+  })
+  , commonjs()
+  , builtins()
+  , globals()
+  , babel({
+        exclude: 'node_modules/**'
+      , babelrc: false
+      , presets: [['@babel/env', { modules: false }]]
+      , plugins: ['@babel/plugin-transform-object-assign']
+      //, externalHelpers: true
+    })
+];
+
+export default [
+   {
+    external: ['dashjs']
+  , input: 'src/main.js'
+  , output: 
+      
+        {
+          file: 'lib/bundle.js'
+        , sourcemap: true
+        , format: 'iife'
+        , name: 'mgPlay'
+        , globals: gglobals
+        }
+        
+  
+  
+  , plugins: gplugins
+}
+, {
+  external: ['dashjs']
+, input: 'src/play.js'
+, output: 
+    {
+        file: 'lib/index.js'
+      , sourcemap: true
+      , format: 'cjs'
+      , globals: gglobals
+      }
+    
+
+
+, plugins: gplugins
+},
+]
+;
