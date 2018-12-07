1ed8f76158ff457f216f4d876056b531a23e00a9
aseduto <aseduto@hotmail.com>
2018-12-07 17:23:13 +0100

    fix some dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -5,58 +5,58 @@ import globals from 'rollup-plugin-node-globals';
 import babel from 'rollup-plugin-babel';
 
 const gglobals = {
-  'dashjs': 'dashjs'
+    'dashjs': 'dashjs'
 };
 
 const gplugins = [
-  resolve({
-      preferBuiltins: true
-      , browser: true
-  })
-  , commonjs()
-  , builtins()
-  , globals()
-  , babel({
+    resolve({
+        preferBuiltins: true
+        , browser: true
+    })
+    , commonjs()
+    , builtins()
+    , globals()
+    , babel({
         exclude: 'node_modules/**'
-      , babelrc: false
-      , presets: [['@babel/env', { modules: false }]]
-      , plugins: ['@babel/plugin-transform-object-assign']
-      //, externalHelpers: true
+        , babelrc: false
+        , presets: [['@babel/env', { modules: false }]]
+        , plugins: ['@babel/plugin-transform-object-assign']
+        //, externalHelpers: true
     })
 ];
 
 export default [
-   {
-    external: ['dashjs']
-  , input: 'src/main.js'
-  , output: 
+    {
+        external: ['dashjs']
+        , input: 'src/main.js'
+        , output: 
       
         {
-          file: 'lib/bundle.js'
-        , sourcemap: true
-        , format: 'iife'
-        , name: 'mgPlay'
-        , globals: gglobals
+            file: 'lib/bundle.js'
+            , sourcemap: true
+            , format: 'iife'
+            , name: 'mgPlay'
+            , globals: gglobals
         }
         
   
   
-  , plugins: gplugins
-}
-, {
-  external: ['dashjs']
-, input: 'src/play.js'
-, output: 
+        , plugins: gplugins
+    }
+    , {
+        external: ['dashjs']
+        , input: 'src/play.js'
+        , output: 
     {
         file: 'lib/index.js'
-      , sourcemap: true
-      , format: 'cjs'
-      , globals: gglobals
-      }
+        , sourcemap: true
+        , format: 'cjs'
+        , globals: gglobals
+    }
     
 
 
-, plugins: gplugins
-},
-]
+        , plugins: gplugins
+    }
+,]
 ;
